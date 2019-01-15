2f8b6baa74fff6392fc0dac36d27e57739d80563
SimpleBinary <40658215+SimpleBinary@users.noreply.github.com>
2019-01-15 15:24:42 +1000

    Add beta file in src

--- src/main.py
+++ src/main.py
@@ -1,30 +1,345 @@
-# The Solar Programming Language
-# https://github.com/Solar-language/Solar
-
-# Licensed under the MIT Licence.
-# See https://github.com/Solar-language/Solar/blob/master/LICENSE.md
-
 import sys
+# inp means input, as in and input are keywords
 
-import lexer
-import parser
-import interpreter
+
+def lexer(inp):
+    current = 0
+    tokens = []
+    while current < len(inp):
+        char = inp[current]
+
+        if char == "(":
+            tokens.append({
+                "type": "paren",
+                "value": "(",
+            })
+            current += 1
+            continue
+
+        if char == ")":
+            tokens.append({
+                "type": "paren",
+                "value": ")",
+            })
+            current += 1
+            continue
+
+        """if char == "[":
+            tokens.append({
+                "type": "bracket",
+                "value": "[",
+            })
+            current += 1
+            continue
+        if char == "]":
+            tokens.append({
+                "type": "bracket",
+                "value": "]",
+            })
+            current += 1
+            continue
+        if char == "{":
+            tokens.append({
+                "type": "curly",
+                "value": "{",
+            })
+            current += 1
+            continue
+        if char == "}":
+            tokens.append({
+                "type": "curly",
+                "value": "}",
+            })
+            current += 1
+            continue
         
-    
-def run(inp):
-    tokens = lexer.Lexer().lex(inp)
-    ast = parser.Parser().parse(tokens)
+        if char == "+":
+            tokens.append({
+                "type": "oper",
+                "value": "+",
+            })
+            current += 1
+            continue
+        if char == "-":
+            tokens.append({
+                "type": "oper",
+                "value": "-",
+            })
+            current += 1
+            continue
+        if char == "/":
+            tokens.append({
+                "type": "oper",
+                "value": "/",
+            })
+            current += 1
+            continue
+        if char == "*":
+            tokens.append({
+                "type": "oper",
+                "value": "*",
+            })
+            current += 1
+            continue
+        
+        if char == "=":
+            
+            tokens.append({
+                "type": "oper",
+                "value": "=",
+            })
+            current += 1
+            continue
+        
+        if char == "~":
+            tokens.append({
+                "type": "oper",
+                "value": "~",
+            })
+            current += 1
+            continue"""
 
+        if char == "`":
+            current += 1
+            char = inp[current]
+            while char != '`':
+                current += 1
+                char = inp[current]
+            current += 1
+            char = inp[current]
+            continue
+
+        whitespace = ["\n", "\t", " "]
+        if char in whitespace:
+            current += 1
+            continue
+
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        if char in numbers:
+            value = ""
+            while char in numbers:
+                value += char
+                current += 1
+                char = inp[current]
+            tokens.append({
+                "type": "number",
+                "value": int(value),
+            })
+            continue
+
+        if char == '"':
+            value = ""
+            current += 1
+            char = inp[current]
+            while char != '"':
+                value += char
+                current += 1
+                char = inp[current]
+            current += 1
+            char = inp[current]
+            tokens.append({
+                "type": "string",
+                "value": value,
+            })
+            continue
+
+        if char == "'":
+            value = ""
+            current += 1
+            char = inp[current]
+            while char != "'":
+                value += char
+                current += 1
+                char = inp[current]
+            current += 1
+            char = inp[current]
+            tokens.append({
+                "type": "string",
+                "value": value,
+            })
+            continue
+
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '$', '+', '-', '/', '*', '%']
+        if char.lower() in letters:
+            value = ""
+            while char.lower() in letters:
+                value += char.lower()
+                current += 1
+                char = inp[current]
+            tokens.append({
+                "type": "name",
+                "value": value,
+            })
+            continue
+
+        raise TypeError("I don't know what this character is: " + char)
+
+    return tokens
+
+
+def parser(tokens):
+
+    global parserCurrent
+
+    def walk():
+        global parserCurrent
+        token = tokens[parserCurrent]
+        if token["type"] == 'number':
+            parserCurrent += 1
+            return {
+                "type": "NumberLiteral",
+                "value": token["value"],
+            }
+
+        if token["type"] == 'string':
+            parserCurrent += 1
+            return {
+                "type": "StringLiteral",
+                "value": token["value"],
+            }
+
+        if token["type"] == 'paren' and token["value"] == "(":
+            parserCurrent += 1
+            token = tokens[parserCurrent]
+            node = {
+                "type": "CallExpression",
+                "name": token["value"],
+                "params": [],
+            }
+            parserCurrent += 1
+            token = tokens[parserCurrent]
+            while token["type"] != "paren" or token["type"] == "paren" and token["value"] != ")":
+                node["params"].append(walk())
+                token = tokens[parserCurrent]
+            parserCurrent += 1
+            return node
+
+        raise TypeError(token["type"])
+    ast = {
+        "type": "Program",
+        "body": [],
+    }
+
+    while parserCurrent < len(tokens):
+        ast["body"].append(walk())
+
+    return ast
+
+
+class Interpreter:
+    def __init__(self):
+        self.environment = {
+            "+": lambda args: add(args),
+            "-": lambda args: subtract(args),
+            "*": lambda args: multiply(args),
+            "/": lambda args: divide(args),
+            "%": lambda args: modulo(args),
+            "int": lambda args: integer(args),
+            "float": lambda args: decimal(args),
+            "str": lambda args: string(args),
+            "put": lambda args: put(args),
+        }
+
+    def call(self, expression):
+        functionName = expression["name"]
+        function = self.environment[functionName]
+
+        params = []
+        for param in expression["params"]:
+            params.append(self.evaluate(param))
+
+        return function(params)
+
+    def evaluate(self, expression):
+        typ = expression["type"]
+
+        if typ == "NumberLiteral":
+            return expression["value"]
+        elif typ == "StringLiteral":
+            return expression["value"]
+        elif typ == "CallExpression":
+            return self.call(expression)
+
+    def interpret(self, program):
+        for expression in program["body"]:
+            self.evaluate(expression)
+
+# Runs the interpreter
+
+
+def run(inp):
+    ast = parser(lexer(inp))
     print("--START AST--")
     print(ast)
     print("--END AST--\n")
+    interpreter = Interpreter()
+    interpreter.interpret(ast)
 
-    interpreter.Interpreter().interpret(ast)
+# Functions in environment
 
-                        
+
+def add(args):
+    assert(len(args) == 2)
+    return args[0] + args[1]
+
+
+def subtract(args):
+    assert(len(args) == 2)
+    return args[0] - args[1]
+
+
+def multiply(args):
+    assert(len(args) == 2)
+    return args[0] * args[1]
+
+
+def divide(args):
+    assert(len(args) == 2)
+    return args[0] / args[1]
+
+
+def modulo(args):
+    assert(len(args) == 2)
+    return args[0] % args[1]
+
+# Integer is used for int in Solar
+
+
+def integer(args):
+    assert(len(args) == 1)
+    return int(args[0])
+
+# Decimal is used for float in Solar
+
+
+def decimal(args):
+    assert(len(args) == 1)
+    return float(args[0])
+
+# String used for str in Solar
+
+
+def string(args):
+    assert(len(args) == 1)
+    return str(args[0])
+
+
+def put(args):  # Equivalent to print in python
+    assert(len(args) == 1)
+    print(args[0])
+    return args[0]
+# End functions in environment
+
+
+parserCurrent = 0
+
+
 def runRepl():
+    global parserCurrent
     while True:
         try:
+            parserCurrent = 0
             run(str(input("solar > ")))
         except KeyboardInterrupt:
             print("\nQuitting...")
@@ -32,12 +347,12 @@ def runRepl():
         except:
             print("Error:", sys.exc_info()[1])
 
-                        
+
 def runFile(filename):
     with open(filename) as sourceFile:
         run(sourceFile.read())
 
-                        
+
 def main():
     if len(sys.argv) == 1:
         runRepl()
@@ -46,5 +361,5 @@ def main():
     else:
         raise RuntimeError("Usage: solar [filename]")
 
-                        
+
 main()
