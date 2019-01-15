3699799f665c1f21bd25fd61584b7a42e0fed2e1
Dandigit <37099129+Dandigit@users.noreply.github.com>
2019-01-15 13:21:09 +1000

    Add notice to interpreter.py

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,3 +1,9 @@
+# The Solar Programming Language
+# https://github.com/Solar-language/Solar
+
+# Licensed under the MIT Licence.
+# See https://github.com/Solar-language/Solar/blob/master/LICENSE.md
+
 class Interpreter:
     def __init__(self):
         self.environment = {
